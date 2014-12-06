58b6b1c0f62a6f9a22fbd55a2f0ca67b7bc4e369
NA-Dronov <nikoman007@gmail.com>
2014-12-06 19:25:32 +0300

    1) Добавлены dll файлы во все проекты (для корректной работы).
    2) В Lection_5 добавлен проект AbstractHtmlCorrector с поддержкой загрузки словаря из БД (используется DictionaryLoader).
    2) Добавлен проект HtmlCorrector_DB, который может подключаться к базе данных Dictionary через App.config.

---
+++ Lection_5/HtmlCorrector_DB/HtmlCorrectorList/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using AbstractHtmlCorrector;
+
+namespace HtmlCorrectorList
+{
+    /// <summary>
+    /// Программный List с поддержкой требуемого интерфейса. Так как методы интерфейса входят в List по
+    /// умолчанию, то определять их не надо.
+    /// </summary>
+    class DictList : List<string>, ICAC<string>
+    { }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HtmlCorrector<DictList> hc = new HtmlCorrector<DictList>();
+            hc.menu();
+        }
+    }
+}
